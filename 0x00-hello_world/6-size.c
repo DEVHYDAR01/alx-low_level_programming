1fcb6cc0f687cec8504fe6b795f04eac9aa22869
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-08-10 17:22:19 -0700

    wow

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: prints the size of various types on the computer it is compiled 
- * and ran it
+ * Description: prints the size of various types on the computer
  *
  * Return: Always 0 (Success)
  */
@@ -13,7 +12,8 @@ int main(void)
 	printf("Size of char: %d byte(s)\n", (int) sizeof(char));
 	printf("Size of int: %d byte(s)\n", (int) sizeof(int));
 	printf("Size of long int: %d byte(s)\n", (int) sizeof(long int));
-	printf("Size of long long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of float: %d byte(s)\n", (int) sizeof(float));
+
 	return (0);
 }
