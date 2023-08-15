40616568d51271c8848f72e99e0ee2aaf0e4824b
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-08-16 00:34:44 +0100

    ok

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints my alphabets from a to in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - This will print the alphabet in lowercase with a newline
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar(alpha);
+}
