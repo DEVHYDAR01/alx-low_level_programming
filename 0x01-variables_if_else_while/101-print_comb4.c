8f2896130e2aa61fefde0293b74e52d9d376a45d
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-08-20 12:39:18 +0100

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 /**
- * main - prints 3 combination of numbers
- *
- * Return: Always 0 (Success)
+ * main - This will print all the combinations of three digits with,
+ * and space followed by new line
+ * 
+ * Return: returns 0
  */
 int main(void)
 {
-	int c, i, k;
+	int dig1, dig2, dig3;
 
-	for (c = '0'; c <= '9'; c++)
+	for (dig1 = 0; dig1 < 9; dig1++)
 	{
-		for (i = '0'; i <= '9'; i++)
+		for (dig2 = dig1 + 1; dig2 < 9; dig2++)
 		{
-			for (k = '0'; k <= '9'; i++)
+			for (dig3 = dig2 + 1; dig3 < 10; dig3++)
 			{
-				if (c < i && i < k)
-				{
-					putchar(c);
-					putchar(i);
-					putchar(k);
-					if (c != '7')
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar((dig1 % 10) + '0');
+				putchar((dig2 % 10) + '0');
+				putchar((dig3 % 10) + '0');
+				if (dig1 == 7 && dig2 == 8 && dig3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
