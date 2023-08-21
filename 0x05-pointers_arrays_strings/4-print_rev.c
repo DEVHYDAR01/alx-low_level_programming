00e17654888e82099aa0b810b92659e80dd87c76
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-08-21 22:36:11 +0100

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - This function will print a string in reverse with a newline
+ * @s: This is the parameter
+ *
+ * Return: This function will return nothing
+ */
+void print_rev(char *s)
+{
+	int l, k, rev;
+
+	for (l = 0; s[l] !='\0'; l++)
+	{
+		_putchar(l);
+	}
+
+	rev = l;
+
+	for (k = rev - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
