5d628e1b7deb31ea3708f3cc626bfd38c708fd60
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-08-24 12:31:55 +0100

    revarr

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array - This function will reverse the content 
+ * of an array of integers
+ * @a: This is the parameter of the array of integers
+ * @n: The is the parameters of the integers to be swapped
+ *
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int arr, swap, hold;
+
+	swap = n - 1;
+
+	for (arr = 0; arr < n / 2; arr++)
+	{
+		hold = a[arr];
+		a[arr] = a[swap];
+		a[swap--] = hold;
+	}
+}
