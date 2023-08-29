5450a6cb8b2cd6e1e35a809d571b2165092c6110
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-08-29 02:34:11 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,43 +1,43 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *  * _strstr - function that locates a substring.
- *   *@haystack: first value -char
- *    *@needle: second value - char
- *     *
- *      * Return: char with result
- *       */
+ * _strstr - This function will locate a substring.
+ * @haystack: This will be the first value char.
+ * @needle: And the second value  char.
+ *
+ * Return: The char will be stored in result
+ */
 char *_strstr(char *haystack, char *needle)
 {
-		int count = 0, a = 0, k, j = 0, l, i = 0;
-			char *p;
+	int up = 0, a = 0, k, dle = 0, l, stk = 0;
+	char *p;
 
-				while (needle[count] != '\0')
-						{
-									count++;
-										}
-					while (haystack[i] != '\0')
-							{
-										i++;
-													}
-						if (count == 0)
-									return (haystack);
-							while (haystack[j] != '\0')
-									{
-												if (haystack[j] == needle[0])
-															{
-																			p = &haystack[j];
-																						l = j;
-																									a = 0;
-																												for (k = 0; k < count; k++)
-																																{
-																																					if (haystack[l] == needle[k])
-																																											a++;
-																																									l++;
-																																												}
-																														}
-														if (a == count)
-																		return (p);
-																j++;
-																	}
-								return ('\0');
+	while (needle[up] != '\0')
+	{
+		up++;
+	}
+	while (haystack[stk] != '\0')
+	{
+		stk++;
+	}
+	if (up == 0)
+		return (haystack);
+	while (haystack[dle] != '\0')
+	{
+		if (haystack[dle] == needle[0])
+		{
+			p = &haystack[dle];
+			l = dle;
+			a = 0;
+			for (k = 0; k < up; k++)
+			{
+				if (haystack[l] == needle[k])
+					a++;
+				l++;
+			}
+		}
+		if (a == up)
+			return (p);
+		dle++;
+	}
+	return ('\0');
 }
