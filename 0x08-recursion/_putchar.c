dd9ec0e9bbe7b219171a5911acdb7a561bb3c4e4
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-08-31 00:32:04 +0100

    puts

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - This function will fill memory with a kind of constant byte
+ * @s: This will be the first value
+ */
+void _puts_recursion(char *s)
+{
+
+	if (s[0] == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		s++;
+		_puts_recursion(s);
+	}
+}
