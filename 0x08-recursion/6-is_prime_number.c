993bdf065dfa1d4197807e4014a7324d5adacb88
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-08-31 03:31:39 +0100

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,26 +2,26 @@
 int calculo_primo(int num_, int inf);
 
 /**
- * is_prime_number - The function will  returns 1 if the input integer is a prime.
+ * is_prime_number - The function will
+ * returns 1 if the input integer is a prime.
  * @n: This is the first value
  *
  * Return: This will return the solution
  */
 int is_prime_number(int n)
 {
-	int a = 2;
+	int arr = 2;
 
 	if (n == 1)
-	{
 		return (0);
-	}
 	if (n < 0)
 		return (0);
-	return (calculo_primo(n, a));
+	return (calculo_primo(n, arr));
 }
 
 /**
- * calculo_primo - The function will return 1 if the input integer is a prime.
+ * calculo_primo - The function will
+ * return 1 if the input integer is a prime.
  * @num_: This is the first value
  * @inf: This is the second value
  *
@@ -30,9 +30,7 @@ int is_prime_number(int n)
 int calculo_primo(int num_, int inf)
 {
 	if (num_ / 2 < inf)
-	{
 		return (1);
-	}
 	if (num_ % inf == 0)
 		return (0);
 	return (calculo_primo(num_, inf + 1));
