4232f58f5c72d17ccc2375aaf9b146cbdd403575
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-07 23:42:19 +0100

    concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *string_nconcat - This is afunction that will concatenates two strings
+ * returned pointer shall point to a newly allocated space
+ * in memory, which contains s1,
+ * followed by the first n bytes of s2, and null terminated.
+ * @s1: This is the string for concatenation
+ * @s2: This is the string from which to transfer.
+ * @n: int type.
+ * Return: pointer to new memory.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int incre, incre1, L1, L2;
+	char *point;
+	int mark = v;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (L1 = 0; s1[L1] != '\0'; L1++)
+		;
+	L2 = 0;
+	while (L2 != '\0')
+	{
+		L2++;
+	}
+	if (mark <= L2)
+	{
+		mark = L2;
+		point = (char *) malloc((L1 + L2 + 1) * sizeof(char));
+	}
+	else
+		point = (char *) malloc(sizeof(char) * (L1 + n + 1));
+	if (point == NULL)
+	{
+		return (NULL);
+	}
+	for (incre = 0; incre < L1; incre++)
+	{
+		point[incre] = s1[incre];
+	}
+	for (incre1 = 0; incre1 < mark; incre1++)
+	{
+		point[incre++] = s2[incre1];
+	}
+	point[incre++] = '\0';
+	return (point);
+}
