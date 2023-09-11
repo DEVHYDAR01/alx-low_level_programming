90af2308a2c9b9733390fdc3d8d55a2c1081fbe0
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-11 23:32:50 +0100

    filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This program will print the name of name
+ * of the file it was compiled from.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
