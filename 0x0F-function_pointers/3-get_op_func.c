a271000736291b652f12b8c9add661799142e398
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-13 23:14:29 +0100

    opfunction

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+/**
+ * get_op_func - This is the function pointer that
+ * will select the correct function to
+ * perform the operation asked by the user
+ * @s: This is the operator given by the user
+ *
+ * Return: This pointer will point to the function that corresponds to the
+ * operator given as a parameter
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int fun;
+
+	for (fun = 0; ops[fun].op; fun++)
+	{
+		if (strcmp(ops[fun].op, s) == 0)
+		{
+			return (ops[fun].f);
+		}
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+/**
+ * main - This function will program that perfroms simple operations
+ * @argc: This is the number of arguments
+ * @argv: This is the array of arguments
+ *
+ * Return: Always 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, res;
+	char k;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	k = *argv[2];
+
+	if ((k == '/' || k == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = func(arg1, arg2);
+
+	printf("%d\n", res);
+
+	return (0);
+}
