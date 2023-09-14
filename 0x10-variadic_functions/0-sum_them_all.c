e5d3faea39aa3faf27bbc90091ebb5cab18aa84a
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-15 00:36:31 +0100

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-
-	if (n == 0)
-	{
-		return 0;
-	}
 	va_list numbers;
 	unsigned int adder;
 	int total;
+
+	if (n == 0)
+	{
+		return (0);
+	}
 
 	total = 0;
 
