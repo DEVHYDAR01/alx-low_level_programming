9b3eb7277faeb5cbb2d81e3c5e3b9ebd61f3bd7a
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-22 02:37:16 +0100

    printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,24 @@
 #include "lists.h"
+/**
+ * print_list - This function will print all
+ * the elements of a list_t list.
+ * @h: This is the  singly linked list.
+ *
+ * Return: The number of elements in the list.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t kap;
+
+	kap = 0;
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		kap++;
+	}
+	return (kap);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,30 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct list_k - This is the singly linked list
+ * @str: string - Dynamic memory allocated (malloc'ed string)
+ * @len: This will be the length of the string
+ * @next: This will point to the next node
+ *
+ * Description: This is my singly linked list node structure
+ */
+typedef struct list_k
+{
+	char *str;
+	unsigned int len;
+	struct list_k *next;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif
