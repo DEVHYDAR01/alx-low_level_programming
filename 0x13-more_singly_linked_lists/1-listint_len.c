197d62cc045a69f969868630111a135806c5a496
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-26 16:05:26 +0100

    this will listinnt len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stlib.h>
+/**
+ * listint_len - This is a function that will
+ * return the number of elements in my list
+ * @h: This is a pointer to listint_t struct
+ * Return: This will return number of elements.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t blocks = 0;
+
+	while (h)
+	{
+		blocks++;
+		h = h->next;
+	}
+	return (blocks);
+}
