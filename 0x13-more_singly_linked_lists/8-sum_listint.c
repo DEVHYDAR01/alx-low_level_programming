f1e589658e9d7d5401c5b5532bbf7e092b79d4f5
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-26 20:19:58 +0100

    the commit will listint

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * sum_listint - This function will return the sum of all data O(n) of the list.
+ * @head: This will be the data type pointer to the head
+ * Return: This is the sum of all nodes.
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *mover_node;
+	int total = 0;
+
+	if (!head)
+	{
+		return (0);
+	}
+
+	mover_node = head;
+	while (mover_node)
+	{
+		total = total + (*mover_node).n;
+		mover_node = mover_node->next;
+	}
+	return (total);
+}
