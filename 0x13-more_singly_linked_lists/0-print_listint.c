47b02c158f4c380c39de68354f22def12e091247
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-26 15:24:54 +0100

    This commit will list my int

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - This function will print all the elements of linked list.
+ * @h: This is our data type to the  pointer of the struct.
+ * Return: The elements of the str rep
+ */
+size_t print_listint(const listint_t *h)
+{
+	int rep = 0;
+	const listint_t	 *new_node = h;
+
+	while (new_node = new_node->next)
+	{
+		printf("%d\n", new_node->n);
+		rep++;
+	}
+	return (rep);
+}
