dcd72965e4e0a0d34cfa41f49a1479ff376f68ea
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-26 19:11:40 +0100

    struct update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,20 +5,17 @@
 #include <string.h>
 #include <stdio.h>
 
-/* This is my struct */
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 /* This are my prototypes */
@@ -33,4 +30,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
 #endif /* End of my inclusion which is guarded */
