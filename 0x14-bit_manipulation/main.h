2ea89e78cacb5660d5216d519ce1949171f3f5ce
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-29 00:04:29 +0100

    This commit will push my print binary file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /* This is the beginning */
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
