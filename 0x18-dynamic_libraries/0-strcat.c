ab3e23051654ee984f9e59e16682e897c8fc46b2
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-11-14 00:24:24 +0100

    This commit will push function and lib

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - This function will concatenate two strings
+ * @dest: This string to append to
+ * @src: This string that will be appended
+ *
+ * Return: Always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c, v;
+
+	c = 0;
+	v = 0;
+
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	while (src[v] != '\0')
+	{
+		dest[c] = src[v];
+		c++;
+		v++;
+	}
+	dest[c] = '\0';
+	return (dest);
+}
