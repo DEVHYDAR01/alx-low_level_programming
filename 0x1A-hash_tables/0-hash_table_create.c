482087a3d9ea55e05e693ca0780c15ef4b252488
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-11-30 23:57:31 +0100

    This commit will push revamp header file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,14 +1,25 @@
 #include "hash_tables.h"
 /**
- * main - check the code for hash
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_create - function to create a hash table
+ * @size: Size of the array
+ * Return: Null for error or a pointer to a new hash table
  */
-int main(void)
+hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
+	unsigned long int i;
 
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-    return (EXIT_SUCCESS);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; ++i)
+	{
+		ht->array[i] = NULL;
+	}
+	return (ht);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,10 +1,6 @@
 #ifndef _hash_tables_
 #define _hash_tables_
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
