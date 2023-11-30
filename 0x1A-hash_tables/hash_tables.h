46ee26f70299b208bb25bce03395a8cde8f8f5d2
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-12-01 00:19:18 +0100

    This commit will push revamp readme

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,11 +1,9 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -16,11 +14,10 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
-
 /**
  * struct hash_table_s - Hash table data structure
  *
@@ -31,10 +28,9 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
-
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
